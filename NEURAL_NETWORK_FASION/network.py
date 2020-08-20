69868f8584cc59e13e04aad06ef465f6ec789dcb
sanath1212 <67041029+sanath1212@users.noreply.github.com>
2020-08-20 23:17:34 +0530

    Add files via upload

---
+++ NEURAL_NETWORK_FASION/network.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+no_images = 5
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images,test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")])
+
+model.compile(optimizer = tf.keras.optimizers.Adam(),
+              loss = tf.keras.losses.sparse_categorical_crossentropy,
+              metrics = ["accuracy"])
+
+checkpoint_path = "WEIGHTS_DATA/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only = True, verbose = 1)
+
+model.load_weights(checkpoint_path)
+model.fit(train_images, train_labels, epochs = 5, validation_data = (test_images, test_labels), callbacks = [cp_callback])
+
+model.load_weights(checkpoint_path)
+loss, acc = model.evaluate(test_images, test_labels)
+print ("retrained model, accuracy: {:5.2f}%".format(100*acc))
+
+prediction = model.predict(test_images)
+
+for i in range(no_images):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap = plt.cm.binary)
+    plt.xlabel("Actual: " + class_names[test_labels[i]])
+    plt.title("AI Prediction: " + class_names[np.argmax(prediction[i])])
+    plt.show()
